d16be6f0da4ec95354d03d751318b04e54455263
EC2 Default User <ec2-user@ip-10-0-10-55.ap-northeast-3.compute.internal>
2022-09-09 11:02:16 +0000

    jsonリクエストに対応。性別と骨格は外部キーを使うように修正。

--- users_app/models.py
+++ users_app/models.py
@@ -4,11 +4,11 @@ class MgtUsersInfo(models.Model):
     user_id = models.CharField(primary_key=True, max_length=255)
     email = models.CharField(unique=True, max_length=255)
     user_name = models.CharField(max_length=255, blank=True, null=True)
-    gender = models.CharField(max_length=255, blank=True, null=True)
+    gender = models.ForeignKey('MgtGenderList', models.DO_NOTHING, blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
     height = models.FloatField(blank=True, null=True)
     weight = models.FloatField(blank=True, null=True)
-    bone_type = models.CharField(max_length=255, blank=True, null=True)
+    bone_type = models.ForeignKey('MgtBoneTypeList', models.DO_NOTHING, blank=True, null=True)
     profile_pic = models.TextField(blank=True, null=True)
     introduction = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
@@ -17,3 +17,19 @@ class MgtUsersInfo(models.Model):
     class Meta:
         managed = False
         db_table = 'mgt_users_info'
+
+class MgtGenderList(models.Model):
+    gender_id = models.IntegerField(primary_key=True)
+    gender = models.CharField(unique=True, max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'mgt_gender_list'
+    
+class MgtBoneTypeList(models.Model):
+    bone_type_id = models.IntegerField(primary_key=True)
+    bone_type = models.CharField(unique=True, max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'mgt_bone_type_list'
--- users_app/views.py
+++ users_app/views.py
@@ -16,39 +16,33 @@ import zipfile
 class Users(TemplateView):
     # ユーザ一覧取得（作成中）
     def get(self,request):
-        hoge = MgtUsersInfo.objects.get(user_id='test1')
+        hoge = MgtUsersInfo.objects.get(user_id='test2')
         json_params = {
-            'users': [
-                {
-                    'hoge1': 'hoge1',
-                    'hoge2': 'hoge2',
-                }
-            ],
-            'result':0x0000,
-            'message':"test",
+            'message': hoge.gender.gender_id
         }
-        status = 200
-        hoge = settings.PITTA_ENV
+        # status = 200
+        # hoge = settings.PITTA_ENV
         json_str = json.dumps(json_params, ensure_ascii=False, indent=2)
-        return HttpResponse(hoge, status=200) 
+        # return HttpResponse(str(vars(hoge.gender)), status=200) 
+        return HttpResponse(json_str, status=200) 
 
     # ユーザ新規登録
     def post(self,request):
         try:
-            user_id = request.POST.get('userId')
-            email = request.POST.get('email')
+            data = json.loads(request.body)
+            user_id = data['userId']
+            email = data['email']
             dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
             dt_now = dt_now.strftime('%Y-%m-%d %H:%M:%S')
-            MgtUsersInfo.objects.create(user_id=user_id, email=email, created_at=dt_now, updated_at=dt_now)
+            user = MgtUsersInfo.objects.create(user_id=user_id, email=email, created_at=dt_now, updated_at=dt_now)
             json_params = {
-                'code':0x0000,
-                'message':"success",
+                "userId": user.user_id,
+                "email": user.email
             }
             status = 200
         except Exception as e:
             json_params = {
-                'code':0x0001,
-                'message':str(e),
+                "message": str(e),
             }
             status = 400
         finally:
@@ -61,9 +55,9 @@ class UserId(TemplateView):
     # ユーザ情報取得(user_id or email)
     def get(self, request, **kwargs):
         try:
-            user_identilier = kwargs['parameter']
-            if MgtUsersInfo.objects.filter(Q(user_id=user_identilier) | Q(email=user_identilier)).exists():
-                user = MgtUsersInfo.objects.get(Q(user_id=user_identilier) | Q(email=user_identilier))
+            user_id = kwargs['parameter']
+            if MgtUsersInfo.objects.filter(user_id=user_id).exists():
+                user = MgtUsersInfo.objects.get(user_id=user_id)
                 pic_url = ''
                 if user.profile_pic is not None:
                     if len(user.profile_pic) != 0:
@@ -75,37 +69,28 @@ class UserId(TemplateView):
                             Params={'Bucket': BUCKET, 'Key': OBJECT},
                             ExpiresIn=300)
                 json_params = {
-                    'user': [
-                        {
-                            'userId': user.user_id,
-                            'email': user.email,
-                            'userName': user.user_name,
-                            'gender': user.gender,
-                            'age': user.age,
-                            'height': user.height,
-                            'weight': user.weight,
-                            'boneType': user.bone_type,
-                            'prifliePic': pic_url,
-                            'introduction': user.introduction,
-                            'createdAt': str(user.created_at),
-                            'updatedAt': str(user.updated_at)
-                        }
-                    ],
-                    'total': 12,
-                    'code':0x0000,
-                    'message':'success',
+                    "userId": user.user_id,
+                    "email": user.email,
+                    "userName": user.user_name,
+                    "genderId": user.gender_id,
+                    "age": user.age,
+                    "height": user.height,
+                    "weight": user.weight,
+                    "boneTypeId": user.bone_type_id,
+                    "prifliePic": pic_url,
+                    "introduction": user.introduction,
+                    "createdAt": str(user.created_at),
+                    "updatedAt": str(user.updated_at)
                 }
                 status = 200
             else:
                 json_params = {
-                    'code':0x0001,
-                    'message':'user not exist',
+                    "message": "user not exist",
                 }
                 status = 404
         except Exception as e:
             json_params = {
-                'code':0x0001,
-                'message':str(e),
+                "message": str(e)
             }
             status = 400
         finally:
@@ -113,26 +98,27 @@ class UserId(TemplateView):
             return HttpResponse(json_str, status=status)
 
     # ユーザ情報更新
-    def post(self, request, **kwargs):
+    def put(self, request, **kwargs):
         try:
             user_id = kwargs['parameter']
             if MgtUsersInfo.objects.filter(user_id=user_id).exists():
                 user = MgtUsersInfo.objects.get(user_id=user_id)
-                for key, value in request.POST.items():
+                data = json.loads(request.body)
+                for key, value in data.items():
                     if key == "email":
                         user.email = value
                     elif key == "userName":
                         user.user_name = value
-                    elif key == "gender":
-                        user.gender = value
+                    elif key == "genderId":
+                        user.gender_id = value
                     elif key == "age":
                         user.age = value
                     elif key == "height":
                         user.height = value
                     elif key == "weight":
                         user.weight = value
-                    elif key == "boneType":
-                        user.bone_type = value
+                    elif key == "boneTypeId":
+                        user.bone_type_id = value
                     elif key == "profilePic":
                         pre_pic = user.profile_pic
                         if len(value) != 0:
@@ -149,25 +135,46 @@ class UserId(TemplateView):
                 dt_now = dt_now.strftime('%Y-%m-%d %H:%M:%S')
                 user.updated_at = dt_now
                 user.save()
-                if pre_pic is not None:
-                    if len(pre_pic) != 0:
-                        if(os.path.isfile('/mnt/goofys/{}'.format(pre_pic))):
-                            os.remove('/mnt/goofys/{}'.format(pre_pic))
+                try:
+                    if pre_pic is not None:
+                        if len(pre_pic) != 0:
+                            if(os.path.isfile('/mnt/goofys/{}'.format(pre_pic))):
+                                os.remove('/mnt/goofys/{}'.format(pre_pic))
+                except:
+                    pass
+                pic_url = ''
+                if user.profile_pic is not None:
+                    if len(user.profile_pic) != 0:
+                        s3_client = boto3.client('s3')
+                        BUCKET = settings.PITTA_ENV
+                        OBJECT = user.profile_pic
+                        pic_url = s3_client.generate_presigned_url(
+                            'get_object',
+                            Params={'Bucket': BUCKET, 'Key': OBJECT},
+                            ExpiresIn=300)
                 json_params = {
-                    'code':0x0000,
-                    'message':'success',
+                    "userId": user.user_id,
+                    "email": user.email,
+                    "userName": user.user_name,
+                    "genderId": user.gender_id,
+                    "age": user.age,
+                    "height": user.height,
+                    "weight": user.weight,
+                    "boneTypeId": user.bone_type.bone_type_id,
+                    "prifliePic": pic_url,
+                    "introduction": user.introduction,
+                    "createdAt": str(user.created_at),
+                    "updatedAt": str(user.updated_at)
                 }
                 status = 200
             else:
                 json_params = {
-                    'code':0x0001,
-                    'message':'user not exist',
+                    "message": "user not exist"
                 }
                 status = 404
         except Exception as e:
             json_params = {
-                'code':0x0001,
-                'message':str(e),
+                "message": str(e),
             }
             status = 400
         finally:
@@ -181,23 +188,20 @@ class UserId(TemplateView):
             if MgtUsersInfo.objects.filter(user_id=user_id).exists():
                 user = MgtUsersInfo.objects.get(user_id=user_id)
                 user.delete()
-                json_params = {
-                    'code':0x0000,
-                    'message':'success',
-                }
-                status = 200
+                status = 204
             else:
                 json_params = {
-                    'code':0x0001,
-                    'message':'user not exist',
+                    "message": "user not exist"
                 }
                 status = 404
         except Exception as e:
             json_params = {
-                'code':0x0001,
-                'message':str(e),
+                "message": str(e),
             }
             status = 400
         finally:
-            json_str = json.dumps(json_params, ensure_ascii=False, indent=2)
-            return HttpResponse(json_str, status=status)
+            if status == 204:
+                return HttpResponse(status=status)
+            else:
+                json_str = json.dumps(json_params, ensure_ascii=False, indent=2)
+                return HttpResponse(json_str, status=status)
